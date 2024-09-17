d66f0dca6c1cbd26ae53b87eec2b2df0d42720c6
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-09-17 14:22:55 +0600

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
             <ul>
                 <li>
                     <Link href="/">
-                        <button className="btn-logo">WEBLOG</button>
+                        <button className="btn-logo">WB</button>
                     </Link>
                 </li>
                 {/* user is signed in and has username */}
