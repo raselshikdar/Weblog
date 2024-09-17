9ec38185d418ce9b64d1831befaa4ec36f0fe932
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-09-17 16:17:25 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -55,19 +55,17 @@ export default function Home(props) {
 
     return (
         <main>
-            <Metatags
-        title="Weblog | Home"
-        description="Get the latest posts on our site"
-      />
-      {!exitedHero && (
+            {/* <Metatags title="Weblog | Home" description="Get the latest posts on our site" /> */}
+
+            {!exitedHero && (
         <div className="card card-info hero">
-          <h2>💡 Welcome to Weblog</h2>
+          <h2>💡 Welcome to Devlog</h2>
           <p>
             Welcome! This app is built with Next.js and Firebase and is loosely
             inspired by Dev.to
           </p>
           <p>
-            Sign up for an 👨‍🎤 account, ✍️ write posts; 💖 hearts other user's posts. All public content is server-rendered and
+            Sign up for an 👨‍🎤 account, ✍️ write posts; 💖 hearts & 💬 comments other's posts. All public content is server-rendered and
             seo optimized. (<a href="/raselshikdar/about-us" aria-lebel="About Us">📖 About</a> • <a href="/raselshikdar/privacy-policy" aria-lebel="Privacy Policy">🛡️ Privacy</a> • <a href="/raselshikdar/terms-of-service" aria-lebel="Terms of Service">📃 Terms</a>)
           </p>
           <span className="exit-hero" onClick={exitHero}>
